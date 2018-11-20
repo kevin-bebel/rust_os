1405742166e83f50661e5d16ebf39b90d06e7e24
%%GITNAME%% <%%GITEMAIL%%>
2018-11-20 23:25:03 +0200

    added in basic kernel logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// Build with command 
+//https://os.phil-opp.com/
+
 /*
  * Since we are creating an Operating system we cannot use anything that rely on another Operating system to function
  * So we are disabling the rust standard Library
@@ -11,12 +14,20 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn main() -> ! {
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
-}
-// On Windows:
-#[no_mangle]
-pub extern "C" fn mainCRTStartup() -> ! {
-    main();
 }
